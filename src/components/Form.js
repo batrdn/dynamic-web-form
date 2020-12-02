8478f4e13b7ec3e79779b593f618a60a5d8ca95b
batrdn <baterdene.tsogoo220@gmail.com>
2020-12-02 19:56:09 +0800

    Removed unnecessary methods

--- src/components/Form.js
+++ src/components/Form.js
@@ -68,36 +68,4 @@ export default class DynamicForm extends HTMLElement {
   unmount() {
     unmountComponentAtNode(this);
   }
-
-  getProps(attributes) {
-    return [...attributes]
-      .filter((attr) => attr.name !== "style")
-      .map((attr) => this.convert(attr.name, attr.value))
-      .reduce((props, prop) => ({ ...props, [prop.name]: prop.value }), {});
-  }
-
-  getEvents() {
-    return Object.values(this.attributes)
-      .filter((key) => /on([a-z].*)/.exec(key.name))
-      .reduce(
-        (events, ev) => ({
-          ...events,
-          [ev.name]: (args) =>
-            this.dispatchEvent(new CustomEvent(ev.name, { ...args }))
-        }),
-        {}
-      );
-  }
-
-  convert(attrName, attrValue) {
-    let value = attrValue;
-    if (attrValue === "true" || attrValue === "false")
-      value = attrValue === "true";
-    else if (!isNaN(attrValue) && attrValue !== "") value = +attrValue;
-    else if (/^{.*}/.exec(attrValue)) value = JSON.parse(attrValue);
-    return {
-      name: attrName,
-      value: value
-    };
-  }
 }
